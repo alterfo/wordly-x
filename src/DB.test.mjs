f44963246be6f9f302b34b2d5c484e3a5924a362
Сидоркин Олег Валентинович <OSidorkin@alfabank.ru>
2024-09-08 11:28:35 +0300

    BREAKING_CHANGE(tauri): not working yet

--- src/DB.test.mjs
+++
@@ -1,7 +0,0 @@
-import {DB} from './DB';
-import * as test from "node:test";
-describe('DB', () => {
-    test('Should be able to create DB', () => {
-        expect(DB).toBeInstanceOf(DB);
-    })
-})
